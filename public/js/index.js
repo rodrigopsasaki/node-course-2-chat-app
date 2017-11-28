2c5472d35dcf0f88a09a0df50fffb34d47479ba1
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2017-11-28 15:12:02 -0200

    Styling the chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 });
 
@@ -43,12 +45,22 @@ locationButton.on('click', function (e) {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton
+    .attr('disabled', 'disabled')
+    .text('Sharing location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton
+      .removeAttr('disabled')
+      .text('Share location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton
+      .removeAttr('disabled')
+      .text('Share location');
     alert('Unable to fetch location');
   });
 });
